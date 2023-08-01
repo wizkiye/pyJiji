7d2135ae84f3efc017da01f475eab8473c67b013
wizkiye <wizkiye@gmail.com>
2023-08-01 15:02:49 +0300

    reorganaize

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyjiji',
+    version='',
+    packages=['jiji', 'jiji.types', 'jiji.errors'],
+    url='',
+    license='',
+    author='Kidus',
+    author_email='',
+    description=''
+)
