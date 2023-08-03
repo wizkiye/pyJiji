926ee4bb219805b52af55fcecb29c5f198c7ec36
wizkiye <wizkiye@gmail.com>
2023-08-03 15:28:34 +0300

    workflow

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(filename):
 
 setup(
     name="jiji",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     url="https://github.com/wizkiye/pyJiji",
     license="",
