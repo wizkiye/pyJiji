a8a802c725fae129d144bec51684ca6bcb9e880e
wizkiye <wizkiye@gmail.com>
2023-08-04 18:50:58 +0300

    version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(filename):
 
 setup(
     name="jiji",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     url="https://github.com/wizkiye/pyJiji",
     license="",
